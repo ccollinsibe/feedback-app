bb19571f89e100a7c27c97faa21d4cb57c094f47
ccollinsibe <ccollinsibe@yahoo.com>
2025-07-10 19:13:13 +0100

    2-6-5

--- src/App.js
+++ src/App.js
@@ -7,29 +7,31 @@ function App(){
         {id:3, text: 'Comment Three'}
     ]
     const loading = false
-    const showComments = false //Change to true or false
+    const showComments = true //Change to true or false
 
     if (loading) return <h1>Loading...</h1>
 
-    return (
-        <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-            {showComments && ( <div className="comments">
+    const commentBlock = <div className="comments">
                 <h3>Comments ({comments.length})</h3>
                 <ul>
                     {comments.map((x,index)=>{
                        return <li key={index}>{x.text}</li>
                     })}
                 </ul>
-            </div>)} 
+            </div>
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            {showComments && commentBlock} 
             <br/>
             {5 + 5}
             <br/>
             {Math.random() * 5 + 5}
             <h3>{title.toUpperCase()}</h3>
         </div>
-        //Used double ampersand and eliminated the null
+        //Put the JSX in its own variable
     )
 }
 
