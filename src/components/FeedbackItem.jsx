a4311418cc9776bd0d6e9a1c056748297713fd3e
ccollinsibe <ccollinsibe@yahoo.com>
2025-07-14 04:42:56 +0100

    3-3-4

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,17 +4,16 @@ function FeedbackItem(){
     const [rating, setRating] = useState(7)
     const [text, setText] = useState("This is an example of a feedback item")
     const handleClick = () => {
-        setRating(10)
-    }
-    const secondClick = () => {
-        setText("Change this")
+        setRating((prev)=>{
+            console.log(prev) 
+            return prev + 1
+        })
     }
     return (
         <div className="card">
             <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
             <button onClick={handleClick}>Click</button>
-            <button onClick={secondClick}>Second Click</button>
         </div>
     )
 }
