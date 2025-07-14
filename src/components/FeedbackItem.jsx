3545312454fda83d0c1849dbef73e5fd15162390
ccollinsibe <ccollinsibe@yahoo.com>
2025-07-14 04:39:28 +0100

    3-3-3

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,11 +3,18 @@ import { useState } from "react"
 function FeedbackItem(){
     const [rating, setRating] = useState(7)
     const [text, setText] = useState("This is an example of a feedback item")
-
+    const handleClick = () => {
+        setRating(10)
+    }
+    const secondClick = () => {
+        setText("Change this")
+    }
     return (
         <div className="card">
             <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={secondClick}>Second Click</button>
         </div>
     )
 }
