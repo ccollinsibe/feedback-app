5e3e146eeab9012d68512765a1046ea3081e50aa
ccollinsibe <ccollinsibe@yahoo.com>
2025-07-16 06:33:27 +0100

    4-5-1

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -24,9 +24,22 @@ function FeedbackForm(){
 
         setText(e.target.value)
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback = {
+                text: text,
+                rating: rating
+            }
+            console.log(newFeedback)
+        }
+    }
+
+
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
                 <RatingSelect select={(rating)=> setRating(rating)}/>
                 <div className='input-group'>
