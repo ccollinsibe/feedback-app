4db36cc36f7482b0d2fdfbf9d7397ec13bb4cec5
ccollinsibe <ccollinsibe@yahoo.com>
2025-07-16 07:14:45 +0100

    4-5-2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid'
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -7,6 +8,11 @@ import FeedbackForm from "./components/FeedbackForm";
 
 function App(){
     const [feedback, setFeedBack] = useState(FeedbackData)
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedBack([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure yoou want to delete?'))
         {
@@ -18,7 +24,7 @@ function App(){
         <>
             <Header/>
             <div className="container">
-                 <FeedbackForm/>
+                 <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 
-function FeedbackForm(){
+function FeedbackForm({handleAdd}){
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
@@ -32,7 +32,8 @@ function FeedbackForm(){
                 text: text,
                 rating: rating
             }
-            console.log(newFeedback)
+            handleAdd(newFeedback)
+            setText('')
         }
     }
 
