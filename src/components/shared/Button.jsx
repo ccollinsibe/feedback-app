ef92bd3f9adbc751db6a8e21fcb8800076471684
ccollinsibe <ccollinsibe@yahoo.com>
2025-07-16 05:13:34 +0100

    4-2-1

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,11 @@
+function Button({children, version = 'primary', type ='button', isDisabled=false}){
+    return (
+        <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+            {children}        
+        </button>
+    )
+}
+
+
+
+export default Button
